b7256b4befb6ca2573159f68842fa832264eda85
Rafael Gonzalez <astrorafael@yahoo.es>
2015-10-06 23:03:02 +0200

    modified setup.py with md->rst and dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,40 @@
 from setuptools import setup, Extension
 import versioneer
 
+# Default description in markdown
+long_description = open('README.md').read()
+ 
+# Converts from makrdown to rst using pandoc
+# and its python binding.
+# Docunetation is uploaded in PyPi when registering
+# by issuing `python setup.py register`
+
+try:
+    import subprocess
+    import pandoc
+ 
+    process = subprocess.Popen(
+        ['which pandoc'],
+        shell=True,
+        stdout=subprocess.PIPE,
+        universal_newlines=True
+    )
+ 
+    pandoc_path = process.communicate()[0]
+    pandoc_path = pandoc_path.strip('\n')
+ 
+    pandoc.core.PANDOC_PATH = pandoc_path
+ 
+    doc = pandoc.Document()
+    doc.markdown = long_description
+ 
+    long_description = doc.rst
+ 
+except:
+    pass
+   
+
+
 classifiers = [
     'Environment :: No Input/Output (Daemon)',
     'Intended Audience :: Science/Research',
@@ -23,10 +57,11 @@ setup(name             = 'emadb',
       author           = 'Rafael Gonzalez',
       author_email     = 'astrorafael@yahoo.es',
       description      = 'A package to collect measurements published by EMA using MQTT and a SQlite database',
-      long_description = open('README.md').read(),
+      long_description = long_description,
       license          = 'MIT',
       keywords         = 'EMA Database Meteorology Astronomy Python RaspberryPi',
       url              = 'http://github.com/astrorafael/emadb/',
       classifiers      = classifiers,
       packages         = ["emadb","emadb.server"],
+      install_requires = ['paho-mqtt']
       )
